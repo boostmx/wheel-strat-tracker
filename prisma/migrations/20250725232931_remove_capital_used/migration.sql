75f989151701d4b38ae391c93088baadbf1a0e8d
hung.nguyen <boostmx@gmail.com>
2025-08-07 21:30:54 -0700

    chore: clean up migrations and new init with latest changes

--- prisma/migrations/20250725232931_remove_capital_used/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `capitalUsed` on the `Trade` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Trade" DROP COLUMN "capitalUsed";
--- prisma/migrations/20250726180604_cascade_trades_ondelete/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Trade" DROP CONSTRAINT "Trade_portfolioId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Trade" ADD CONSTRAINT "Trade_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
