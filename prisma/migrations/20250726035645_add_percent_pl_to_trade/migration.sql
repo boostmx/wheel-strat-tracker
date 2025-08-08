ebedeac9901718db2668e1d452d16d0f705b59d8
Hung Nguyen <31421235+boostmx@users.noreply.github.com>
2025-08-07 21:32:26 -0700

    chore: clean up migrations and new init with latest changes (#2)

--- prisma/migrations/20250726035645_add_percent_pl_to_trade/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Trade" ADD COLUMN     "percentPL" DOUBLE PRECISION;
--- prisma/migrations/20250808042717_init/migration.sql
+++ prisma/migrations/20250808042717_init/migration.sql
@@ -7,6 +7,12 @@ CREATE TYPE "TradeStatus" AS ENUM ('open', 'closed');
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "bio" TEXT,
+    "avatarUrl" TEXT,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -19,6 +25,8 @@ CREATE TABLE "Portfolio" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "startingCapital" DOUBLE PRECISION NOT NULL,
+    "currentCapital" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Portfolio_pkey" PRIMARY KEY ("id")
@@ -34,16 +42,20 @@ CREATE TABLE "Trade" (
     "contracts" INTEGER NOT NULL,
     "contractPrice" DOUBLE PRECISION NOT NULL,
     "entryPrice" DOUBLE PRECISION,
-    "capitalUsed" DOUBLE PRECISION,
     "status" "TradeStatus" NOT NULL DEFAULT 'open',
     "closedAt" TIMESTAMP(3),
     "closingPrice" DOUBLE PRECISION,
     "premiumCaptured" DOUBLE PRECISION,
+    "percentPL" DOUBLE PRECISION,
     "portfolioId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "notes" TEXT,
 
     CONSTRAINT "Trade_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
@@ -52,4 +64,4 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 ALTER TABLE "Portfolio" ADD CONSTRAINT "Portfolio_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Trade" ADD CONSTRAINT "Trade_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Trade" ADD CONSTRAINT "Trade_portfolioId_fkey" FOREIGN KEY ("portfolioId") REFERENCES "Portfolio"("id") ON DELETE CASCADE ON UPDATE CASCADE;
