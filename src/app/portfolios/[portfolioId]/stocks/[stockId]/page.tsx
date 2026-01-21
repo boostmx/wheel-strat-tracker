bfe1c506bc7d3ebe36230308af39b14621b64c43
hung.nguyen <boostmx@gmail.com>
2026-01-21 11:10:52 -0800

    fixed portfolioId on stock details page

--- src/app/portfolios/[portfolioId]/stocks/[stockId]/page.tsx
+++ src/app/portfolios/[portfolioId]/stocks/[stockId]/page.tsx
@@ -1,10 +1,10 @@
 import StockDetailPageClient from "@/features/stocks/components/StockDetailPageClient";
 
 export default async function Page(props: {
-  params: Promise<{ id: string; stockId: string }>;
+  params: Promise<{ portfolioId: string; stockId: string }>;
 }) {
   const params = await props.params;
-  const portfolioId = params.id;
+  const portfolioId = params.portfolioId;
   const stockId = params.stockId;
 
   return <StockDetailPageClient portfolioId={portfolioId} stockId={stockId} />;
